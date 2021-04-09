5f7fe26826c313c57c54b5304768782e0d7ad739
Burov Yura <yuraburov18@gmail.com>
2021-04-10 00:04:32 +0300

    Slightly changed the navbar construction logic (Немного изменил логику построение navbar)

--- AdminPanel/adminpanel-app/src/components/Conntent.js
+++ AdminPanel/adminpanel-app/src/components/Conntent.js
@@ -5,33 +5,17 @@ import{Browser, BrowserRouter, Route, Switch} from 'react-router-dom';
 import { Users } from '../ConntentInfo/Users';
 import { Servies } from '../ConntentInfo/Servies';
 
-
-const Logout = () => {
-    localStorage.removeItem('user');
-    window.location.reload();     
-}
-
 function Conntent() {
-
     return (
       <div className="App">
         <BrowserRouter>
-          <div className="container">
-            <h2>Welcome, <span></span></h2>            
-            <button onClick={Logout}>Logout</button>
-            <h3 className="m-3 d-flex justify-content-center">
-              React JS with Web api Demo
-            </h3>
-            
             <Navigation/>
-
             <Switch>
               <Route path='/news' component={News} exact/>
               <Route path='/events' component={Events} exact/>
               <Route path='/users' component={Users} exact/>
               <Route path='/servies' component={Servies} exact/>
             </Switch>
-          </div>
         </BrowserRouter>
       </div>
     );
--- AdminPanel/adminpanel-app/src/components/Navigation.js
+++ AdminPanel/adminpanel-app/src/components/Navigation.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { Navbar, Nav } from 'react-bootstrap';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
+const Logout = () => {
+  localStorage.removeItem("user");
+  window.location.reload();
+};
 export class Navigation extends Component {
-    render() {
-        return (
-            <Navbar bg="dark" expand="lg">
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav>
-                        <NavLink className="d-inline p-2 bg-dark text-white" to="/events">Events</NavLink>
-                        <NavLink className="d-inline p-2 bg-dark text-white" to="/news">News</NavLink>
-                        <NavLink className="d-inline p-2 bg-dark text-white" to="/users">Users</NavLink>
-                        <NavLink className="d-inline p-2 bg-dark text-white" to="/servies">Servies</NavLink>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        )
-    }
+  render() {
+    return (
+      <Navbar className="navbar navbar-expand-lg navbar-light bg-light" expand="lg">
+        <Navbar.Toggle aria-controls="collapse navbar-collapse" />
+        <Navbar.Collapse>
+          <Nav>
+            <NavLink className="nav-link" to="/events">
+              Events
+            </NavLink>
+            <NavLink className="nav-link" to="/news">
+              News
+            </NavLink>
+            <NavLink className="nav-link" to="/users">
+              Users
+            </NavLink>
+            <NavLink className="nav-link" to="/servies">
+              Servies
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+        <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+            </li>
+            <li className="nav-item">
+              <button type="button" className="btn btn-light pull-left" onClick={Logout}>
+                Sign out
+              </button>
+            </li>
+          </ul>
+        </div>
+      </Navbar>
+    );
+  }
 }
