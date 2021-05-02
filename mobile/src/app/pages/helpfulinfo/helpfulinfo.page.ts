1772ecc897d1234f096203e7aa9b68a2461da9c2
Burov Yura <yuraburov18@gmail.com>
2021-05-02 13:31:34 +0300

    Test output useful documentation page (Тестовый вывод страница с полезной документацией)

--- mobile/src/app/pages/helpfulinfo/helpfulinfo.page.ts
+++ mobile/src/app/pages/helpfulinfo/helpfulinfo.page.ts
@@ -7,19 +7,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./helpfulinfo.page.scss'],
 })
 export class HelpfulinfoPage implements OnInit {
-  userAnswer: any;
-
+  infos: any;
   constructor(
     private router: Router,
   ) { }
 
   ngOnInit() {
+    let info1 = [
+      { id: "0", title: "Подготовка заявление Р21001 на открытие ИП"},
+      { id: "1", title: "Какие требуются документы для открытия ИП? "},
+    ]
+    this.infos = info1;
+    console.log(info1)
   }
 
-  onClick(event){
-    this.userAnswer = event;
+  onClick(event) {
     console.log(event);
-    this.router.navigateByUrl('/moredetailsinfo', {state:{title : event}})
+    this.router.navigateByUrl('/moredetailsinfo', { state: { id: event } })
   }
 
 }
--- mobile/src/app/pages/moredetailsinfo/moredetailsinfo.page.ts
+++ mobile/src/app/pages/moredetailsinfo/moredetailsinfo.page.ts
@@ -9,14 +9,24 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./moredetailsinfo.page.scss'],
 })
 export class MoredetailsinfoPage implements OnInit {
-  state$: Observable<object>;
+  state: Observable<object>;
+  moreInfs: any;
 
-  constructor(public activatedRoute: ActivatedRoute) {}
+  constructor(public activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.state$ = this.activatedRoute.paramMap
+    let moreInf = [
+      {
+        g: '<p><b>Шаг 1. Подготовьте заявление Р21001 на открытие ИП</b></p>' +
+          '<p>В настоящее время набирают популярность онлайн сервисы по подготовке документов на регистрацию ИП и ООО. Основным их преимуществом, помимо быстроты и удобства, является правильное заполнение заявления о государственной регистрации физического лица в качестве индивидуального предпринимателя по форме P21001, ошибка в котором может повлечь за собой отказ в регистрации ИП. Вы вводите необходимые данные непосредственно через страницы сайта и на выходе получаете готовые к распечатке и подаче на государственную регистрацию ИП документы'
+      },
+      { g: "zz" },
+    ]
+    this.state = this.activatedRoute.paramMap
       .pipe(map(() => window.history.state))
-      console.log(window.history.state);
+    console.log("ww", window.history.state.id);
+    console.log(moreInf[0])
+    this.moreInfs = moreInf[window.history.state.id].g;
   }
 
 }
