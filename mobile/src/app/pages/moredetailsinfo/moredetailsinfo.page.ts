57ca10542143aae551b9140ed264d303058634b7
Burov Yura <yuraburov18@gmail.com>
2021-05-04 20:42:16 +0300

    Added a json file where articles will be stored and added test examples of classes to change the color palette of information design (Добавил json файл где будут храниться статьи и добавил тестовый примеры классов для изменения цветовой палитры оформления информации)

--- mobile/src/app/pages/moredetailsinfo/moredetailsinfo.page.ts
+++ mobile/src/app/pages/moredetailsinfo/moredetailsinfo.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import info from './articles/articlesInfo.json';
 
 @Component({
   selector: 'app-moredetailsinfo',
@@ -11,22 +12,16 @@ import { map } from 'rxjs/operators';
 export class MoredetailsinfoPage implements OnInit {
   state: Observable<object>;
   moreInfs: any;
-
+  infoList = info;
   constructor(public activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    let moreInf = [
-      {
-        g: '<p><b>Шаг 1. Подготовьте заявление Р21001 на открытие ИП</b></p>' +
-          '<p>В настоящее время набирают популярность онлайн сервисы по подготовке документов на регистрацию ИП и ООО. Основным их преимуществом, помимо быстроты и удобства, является правильное заполнение заявления о государственной регистрации физического лица в качестве индивидуального предпринимателя по форме P21001, ошибка в котором может повлечь за собой отказ в регистрации ИП. Вы вводите необходимые данные непосредственно через страницы сайта и на выходе получаете готовые к распечатке и подаче на государственную регистрацию ИП документы'
-      },
-      { g: "zz" },
-    ]
     this.state = this.activatedRoute.paramMap
       .pipe(map(() => window.history.state))
     console.log("ww", window.history.state.id);
-    console.log(moreInf[0])
-    this.moreInfs = moreInf[window.history.state.id].g;
+
+    console.log(this.infoList[0]);
+    this.moreInfs = this.infoList[window.history.state.id].info;
   }
 
 }
